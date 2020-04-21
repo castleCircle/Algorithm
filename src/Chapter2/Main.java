96fc7167826a3ee21b1ab66edeffb488e386af36
yoonemong <yoonemong@naver.com>
2020-04-21 22:57:42 +0900

    start 아자아자

---
+++ src/Chapter2/Main.java
@@ -0,0 +1,29 @@
+package Chapter2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int min , index =0 , temp;
+        int[] array = {1,10,5,8,7,6,4,3,2,9};
+        for(int i=0;i<10;i++){
+            min = 9999;
+            for(int j=i;j<10;j++){
+                if(min>array[j]){
+                    min = array[j];
+                    index = j;
+                }
+            }
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
