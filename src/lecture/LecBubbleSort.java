f2ec31c108aa422fd859a2427ba0f4607388bd5b
yoonemong <yoonemong@naver.com>
2020-11-13 01:17:48 +0900

    버블정렬 , 선택정렬

--- src/lecture/LecBubbleSort.java
+++ src/lecture/LecBubbleSort.java
@@ -1,4 +1,22 @@
 package lecture;
 
 public class LecBubbleSort {
+    public static void main(String[] args) {
+        int[] data = {2,5,1,9,4,8,6,7,3};
+
+        for(int i=0;i<data.length;i++){
+            for(int j=0;j<data.length-1-i;j++){
+                if(data[j] > data[j+1]){
+                    int temp = data[j+1];
+                    data[j+1] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
+
+        for(int i=0;i<data.length;i++){
+            System.out.println(data[i]);
+        }
+
+    }
 }
--- src/lecture/LecSelectionSort.java
+++ src/lecture/LecSelectionSort.java
@@ -1,4 +1,35 @@
 package lecture;
 
+/**
+ 선택정렬
+ */
+
 public class LecSelectionSort {
+    public static void main(String[] args) {
+        int[] data = {2,5,1,9,4,8,6,7,3};
+
+        for(int i=0;i<data.length;i++){
+            int index=0;
+            int min = 9999;
+
+            for(int j=i;j<data.length;j++){
+                if(min > data[j]){
+                    min = data[j];
+                    index = j;
+                }
+            }
+
+            int temp = data[index];
+            data[index] = data[i];
+            data[i] = temp;
+        }
+
+
+        for(int i=0;i<data.length;i++){
+            System.out.print(data[i]);
+        }
+
+
+    }
+
 }
