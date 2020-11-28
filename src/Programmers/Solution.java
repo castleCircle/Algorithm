20e8fb27a16504868c5edbdd0d868f942cc49f9f
yoonemong <yoonemong@naver.com>
2020-11-29 01:19:05 +0900

    프로그래머스 완주하지 못한 선수 해결

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,14 +1,48 @@
 package Programmers;
 
+import java.util.Arrays;
+
+//Programmers 완수하지 못한 선수
 public class Solution {
 
     public static void main(String[] args) {
 
+       String[] participant ={"mislav","stanko","mislav","ana"};
+        String[] completion ={"stanko","ana","mislav"};
+
+//        String[] participant ={"leo","kiki","eden"};
+//        String[] completion ={"eden","kiki"};
+
+        String member = solution(participant,completion);
+        System.out.println(member);
+
     }
 
     public static String solution(String[] participant,String[] completion){
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i=0;
+        String temp = "";
         String answer = "";
+        while(i<completion.length){
+            if(!completion[i].equals(participant[i])){
+                temp = participant[i];
+                break;
+            }
+            i++;
+        }
+
+        if(!"".equals(temp)){
+            answer = temp;
+        }else{
+            answer = participant[participant.length-1];
+        }
+
+
         return answer;
+
     }
 
 
--- src/Test.java
+++ src/Test.java
@@ -10,6 +10,23 @@ public class Test {
        int mid1 = (start + end) >>> 1;
        System.out.println(mid1);
 
+
+       int[] a = new int[7];
+       a[0] = 5;
+        a[1] = 2;
+        a[2] = 4;
+        a[3] = 1;
+        a[4] = 2;
+        a[5] = 4;
+        a[6] = 5;
+
+               int result= 0;
+               for(int number : a){
+                   result ^= number;
+               }
+
+        System.out.println(result);
+
     }
 
 }
