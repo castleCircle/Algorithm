b5d1fed2e43606ee67fdb57bb37808683907248f
yoonemong <yoonemong@naver.com>
2021-08-05 00:02:12 +0900

    no message

--- src/Programmers/level2/최댓값과최솟값.java
+++ src/Programmers/level2/최댓값과최솟값.java
@@ -1,4 +1,18 @@
 package Programmers.level2;
 
+import java.util.Arrays;
+
 public class 최댓값과최솟값 {
+
+    public static void main(String[] args) {
+        System.out.println(solution("-1 -2 -3 -4"));
+    }
+
+    public static String solution(String s) {
+        String[] s1 = s.split(" ");
+        int[] ints = Arrays.stream(s1).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(ints);
+        return ints[0] + " " +ints[ints.length-1];
+    }
+
 }
